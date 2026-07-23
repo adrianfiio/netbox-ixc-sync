1739bb196e687c479df2eb2d79e4b445aa8e1ac9
adrianfiio <45978851+adrianfiio@users.noreply.github.com>
2026-07-23 15:32:14 -0300

    Remove 'host' and 'token' fields from IXCConfigForm

--- netbox_ixc_sync/forms.py
+++ netbox_ixc_sync/forms.py
@@ -5,4 +5,4 @@ from .models import IXCConfig
 class IXCConfigForm(NetBoxModelForm):
     class Meta:
         model = IXCConfig
-        fields = ('name', 'host', 'token', 'verify_ssl', 'prefix', 'vrf_name', 'tags')
+        fields = ('name', 'prefix', 'vrf_name', 'tags')
