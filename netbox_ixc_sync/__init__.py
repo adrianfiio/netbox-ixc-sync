60e2d377f2ab18392fcf295cf01da61018f4309f
adrianfiio <45978851+adrianfiio@users.noreply.github.com>
2026-07-23 15:38:21 -0300

    Update __init__.py

--- netbox_ixc_sync/__init__.py
+++ netbox_ixc_sync/__init__.py
@@ -5,13 +5,11 @@ class IXCSyncConfig(PluginConfig):
     name = 'netbox_ixc_sync'
     verbose_name = 'IXCSoft Sync'
     description = 'Sincroniza clientes e IPs fixos do IXCSoft com o NetBox'
-    version = '0.2.0'
+    version = '0.2.1'
     author = 'adrianfiio'
     base_url = 'ixc-sync'
     min_version = '4.6.0'
 
-    # Valores padrão. As credenciais reais vêm do configuration.py
-    # (que por sua vez lê variáveis de ambiente).
     default_settings = {
         'ixc_host': '',
         'ixc_token': '',
