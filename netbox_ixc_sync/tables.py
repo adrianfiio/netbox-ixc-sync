25f0580dc2e815c65646aada8fcd812979ca8db2
adrianfiio <45978851+adrianfiio@users.noreply.github.com>
2026-07-23 15:57:31 -0300

    Update tables.py

--- netbox_ixc_sync/tables.py
+++ netbox_ixc_sync/tables.py
@@ -7,11 +7,12 @@ class IXCConfigTable(NetBoxTable):
     name = tables.Column(linkify=True)
     prefix = tables.Column(verbose_name='Bloco')
     vrf_name = tables.Column(verbose_name='VRF')
+    remove_orphans = columns.BooleanColumn(verbose_name='Remove órfãos')
 
     class Meta(NetBoxTable.Meta):
         model = IXCConfig
-        fields = ('pk', 'id', 'name', 'host', 'prefix', 'vrf_name', 'verify_ssl')
-        default_columns = ('name', 'host', 'prefix', 'vrf_name')
+        fields = ('pk', 'id', 'name', 'prefix', 'vrf_name', 'remove_orphans')
+        default_columns = ('name', 'prefix', 'vrf_name', 'remove_orphans')
 
 
 class SyncLogTable(NetBoxTable):
@@ -23,9 +24,10 @@ class SyncLogTable(NetBoxTable):
         model = SyncLog
         fields = (
             'pk', 'id', 'config', 'timestamp', 'success',
-            'criados', 'atualizados', 'ignorados', 'total_ixc', 'mensagem',
+            'criados', 'atualizados', 'ignorados', 'removidos',
+            'total_ixc', 'mensagem',
         )
         default_columns = (
             'config', 'timestamp', 'success',
-            'criados', 'atualizados', 'ignorados',
+            'criados', 'atualizados', 'ignorados', 'removidos',
         )
